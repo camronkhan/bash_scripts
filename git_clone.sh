2f8fb9ec58523e2fd7c19845b01db26c64a0b742
Camron Khan <camron.khan@gmail.com>
2017-11-05 10:06:41 -0600

    check for empty dirname

--- git_clone.sh
+++ git_clone.sh
@@ -11,8 +11,8 @@ if [ -z "$1" || -z "$2" ]; then
   echo "ERROR: GitHub user or repo name not provided."
 else
   if [ -z "$3" ]; then
-    git clone ${GITHUB}${1}"/"${2}".git" ${3}
-  else
     git clone ${GITHUB}${1}"/"${2}".git"
+  else
+    git clone ${GITHUB}${1}"/"${2}".git" ${3}
   fi
 fi
