a3ce80521f38ae9b0f031959f54cb372c5a5959d
Camron Khan <camron.khan@gmail.com>
2017-11-05 13:34:38 -0600

    openvpn install

---
+++ openvpn_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://github.com/Angristan/OpenVPN-install
+
+wget https://raw.githubusercontent.com/Angristan/OpenVPN-install/master/openvpn-install.sh
+chmod +x openvpn-install.sh
+./openvpn-install.sh
