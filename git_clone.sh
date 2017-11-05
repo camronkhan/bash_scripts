7fd8739fe9790cdb2ff6a721e2a555b75ce65cf8
Camron Khan <camron.khan@gmail.com>
2017-11-05 09:57:58 -0600

    git clone script

---
+++ git_clone.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# clone git repo
+# $1 = user name
+# $2 = repo name
+# $3 = directory name
+
+GITHUB=https://www.github.com/
+
+if [ -z "$1" || -z "$2" ]; then
+  echo "ERROR: GitHub user or repo name not provided."
+else
+  git clone ${GITHUB}${1}"/"${2}".git "${3}
+fi
