235d9bd1960d7a10e1a665c6c3311f65f167619f
Camron Khan <camron.khan@gmail.com>
2017-11-05 10:02:52 -0600

    add branch for dirname

--- git_clone.sh
+++ git_clone.sh
@@ -10,5 +10,9 @@ GITHUB=https://www.github.com/
 if [ -z "$1" || -z "$2" ]; then
   echo "ERROR: GitHub user or repo name not provided."
 else
-  git clone ${GITHUB}${1}"/"${2}".git" ${3}
+  if [ -z "$3" ]; then
+    git clone ${GITHUB}${1}"/"${2}".git" ${3}
+  else
+    git clone ${GITHUB}${1}"/"${2}".git"
+  fi
 fi
