912f328284ac74a37c4a545e9a9c0cc1434f4cfe
Camron Khan <camron.khan@gmail.com>
2017-11-05 13:50:29 -0600

    add comment

--- openvpn_install.sh
+++ openvpn_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # https://github.com/Angristan/OpenVPN-install
+# must be executed as root user
 
 wget https://raw.githubusercontent.com/Angristan/OpenVPN-install/master/openvpn-install.sh
 chmod +x openvpn-install.sh
